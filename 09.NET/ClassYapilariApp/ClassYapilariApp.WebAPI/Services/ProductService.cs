b607477cfb384cbce794bff00f84739071a62cc3
ramazankurt56 <93058427+ramazankurt56@users.noreply.github.com>
2024-02-24 22:24:36 +0300

    24.02.2024

---
+++ 09.NET/ClassYapilariApp/ClassYapilariApp.WebAPI/Services/ProductService.cs
@@ -0,0 +1,73 @@
+﻿using ClassYapilariApp.WebAPI.DTOs;
+using ClassYapilariApp.WebAPI.Models;
+using ClassYapilariApp.WebAPI.Utilities;
+
+namespace ClassYapilariApp.WebAPI.Services;
+
+public class ProductService
+{
+    public static List<Product> products = new();
+
+    public Result<List<Product>> GetAll()
+    {
+        return products;
+    }
+
+    public Result<Guid> Add(AddProductDto request)
+    {
+        Product product = new(
+            name: request.Name,
+            quantity: request.Quantity,
+            price: request.Price);
+
+        products.Add(product);
+
+        return "Ürün ekleme işlemi başarılı";
+    }
+
+    public Result<Guid> Selling(Guid productId, int quantity)
+    {
+        Product? product = products.FirstOrDefault(p => p.Id == productId);
+        if (product is null)
+        {
+            return (500, "Ürün bulunamadı!");
+        }
+
+        product.Quantity -= quantity;
+
+        if (product.Quantity < 0)
+        {
+            product.Quantity += quantity;
+
+            string errorMessage = product.Name + " ürün stoku satıştan sonra eskiye düşeceği için satış iptal edildi. Stok ekleyip satışı tekrar yapın!";
+            return (500, errorMessage);
+        }
+
+        return "Satış işlemi başarıyla tamamlandı";
+    }
+
+    public Result<Guid> AddStock(Guid productId, int quantity)
+    {
+        Product? product = products.FirstOrDefault(p => p.Id == productId);
+        if (product is null)
+        {
+            return (500,"Ürün bulunamadı!");
+        }
+
+
+        product.Quantity += quantity;
+
+        return "Ürün adedi başarıyla güncellendi";
+    }
+
+    public Result<List<ProdutReportListDto>> GetProductListForReport()
+    {
+        var reportList = products.Select(s => new ProdutReportListDto()
+        {
+            ProductName = s.Name,
+            ProductQuantity = s.Quantity
+        }).ToList();
+        
+        return reportList;
+    }
+}
---
+++ 09.NET/ClassYapilariApp/ClassYapilariApp.WebAPI/Utilities/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace ClassYapilariApp.WebAPI.Utilities;
+
+public class Result<T>
+{//implicit => bunu araştırın
+    
+    public Result(T data)
+    {
+        Data = data;
+    }
+    public Result(string message)
+    {
+        Message = message;
+    }
+    public Result(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        IsSuccess = false;
+        Message = message;
+    }
+
+    public Result(int statusCode, List<string> messages)
+    {
+        Messages = messages;
+        StatusCode = statusCode;
+    }
+
+    public T? Data { get; set; }
+    public int StatusCode { get; set; } = 200;
+    public bool IsSuccess { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+    public List<string> Messages { get; set; } = new();   
+
+    public static implicit operator Result<T>(T data)
+    {
+        return new Result<T>(data);
+    }
+
+    public static implicit operator Result<T>(string message)
+    {
+        return new Result<T>(message);
+    }
+
+    public static implicit operator Result<T>((int, string) parameters)
+    {
+        return new Result<T>(parameters.Item1, parameters.Item2);
+    }
+}
